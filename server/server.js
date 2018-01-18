53b474624347dcb718539b182ea9e3e84a0b36d7
Dpartipilo <Dpartipilo@hotmail.com>
2018-01-18 16:50:38 +0000

    Added database URL

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
+require('dotenv').config({
+  path: `./.${process.env.NODE_ENV}.env`
+});
+
 //## Server
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+process.env.NODE_ENV;
 //##Database
-const config = require('../config');
-const db = config.DB[process.env.NODE_ENV] || process.env.DB;
+
 const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
@@ -15,23 +18,23 @@ app.use(bodyParser.json());
 //##API-Route
 const apiRouter = require('../routes/api');
 
-mongoose.connect(db, {useMongoClient: true})
-  .then(() => console.log('successfully connected to', db))
-  .catch(err => console.error('connection failed', err));
+mongoose.connect(process.env.DB_URI, { useMongoClient: true })
+  .then(() => console.log('successfully connected to the database'))// eslint-disable-line no-console
+  .catch(err => console.error('connection failed', err));// eslint-disable-line no-console
 
 app.use('/api', apiRouter);
 
 app.use((err, req, res, next) => {
-  if(err.type === 400) res.status(400).send(err, {msg: 'invalid id'});
-  else(next(err));
+  if (err.type === 400) res.status(400).send(err, { msg: 'invalid id' });
+  else (next(err));
 });
 
 app.use((err, req, res, next) => {
-  if(err.type === 404) res.status(404).send(err, {msg: 'page not found'});
-  else(next(err));
+  if (err.type === 404) res.status(404).send(err, { msg: 'page not found' });
+  else (next(err));
 });
 
-app.use((err, req, res, next) => res.status(500).send({err}));
+app.use((err, req, res) => res.status(500).send({ err }));
 
 
 module.exports = app;
