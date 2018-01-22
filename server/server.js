5a06db80af00d7999ab7f18c7e50f959e5e42e9a
Dpartipilo <Dpartipilo@hotmail.com>
2018-01-22 19:49:37 +0000

    Added [cors] dependency

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 /*eslint-disable no-console*/
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
-require('dotenv').config({
+if (process.env.NODE_ENV !== 'production') require('dotenv').config({
   path: `./.${process.env.NODE_ENV}.env`
 });
 
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-process.env.NODE_ENV;
+const cors = require('cors');
 const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
+const app = express();
 const apiRouter = require('../routes/api');
 
 mongoose.connect(process.env.DB_URI, { useMongoClient: true })
@@ -18,20 +18,21 @@ mongoose.connect(process.env.DB_URI, { useMongoClient: true })
   .catch(err => console.error('connection failed', err));
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api', apiRouter);
 
-app.use((err, req, res, next) => {
-  if (err.type === 400) res.status(400).send(err, { msg: 'invalid id' });
-  else (next(err));
+app.use('/*', (req, res) => {
+  res.status(404).send({ msg: 'Page not found' });
 });
 
 app.use((err, req, res, next) => {
-  if (err.type === 404) res.status(404).send(err, { msg: 'page not found' });
-  else (next(err));
+  if(err.status === 404) return res.status(404).send({message: err.message});
+  if(err.status === 400) return res.status(400).send({message: err.message});
+  else return next(err);
 });
-
-app.use((err, req, res) => res.status(500).send({ err }));
-
+app.use((err, req, res) => {
+  res.status(500).send({err});
+});
 
 module.exports = app;
