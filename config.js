e1171a07cb4850c690b8e6fff60f0d2234592b4d
Dpartipilo <Dpartipilo@hotmail.com>
2018-02-04 21:40:28 +0000

    config changes for production mode.

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  DB:{
-    test: 'mongodb://localhost/NCnews_test',
-    dev: 'mongodb://localhost/NCnews'
+  DB: {
+    test: "mongodb://localhost/NCnews_test",
+    dev: "mongodb://localhost/NCnews"
   },
   PORT: {
     test: 3090,
-    dev: 3050
+    dev: 3050,
+    production: 3001
   }
 };
--- server/server.js
+++ server/server.js
@@ -1,31 +1,30 @@
 /*eslint-disable no-console*/
-if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
-if (process.env.NODE_ENV !== 'production')
-  require('dotenv').config({
-    path: `./.${process.env.NODE_ENV}.env`
-  });
+if (!process.env.NODE_ENV) process.env.NODE_ENV = "dev";
+require("dotenv").config({
+  path: `./.${process.env.NODE_ENV}.env`
+});
 
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 
 const app = express();
-const apiRouter = require('../routes/api');
+const apiRouter = require("../routes/api");
 
 mongoose
   .connect(process.env.DB_URI, { useMongoClient: true })
   .then(() =>
     console.log(`successfully connected to "${process.env.NODE_ENV}" database`)
   )
-  .catch(err => console.error('connection failed', err));
+  .catch(err => console.error("connection failed", err));
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', apiRouter);
-app.use('/*', (req, res) => {
-  res.status(404).send({ message: 'Page not found' });
+app.use("/api", apiRouter);
+app.use("/*", (req, res) => {
+  res.status(404).send({ message: "Page not found" });
 });
 
 app.use((err, req, res, next) => {
